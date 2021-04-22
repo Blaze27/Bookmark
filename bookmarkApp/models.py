3354918929ed0d7a9d6cdabb5c97b623f7459d7a
Rahul Sharma <bbblaze27@gmail.com>
2021-04-22 15:12:21 +0530

    Create models and migrate to database

--- bookmarkApp/models.py
+++ bookmarkApp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from taggit.managers import TaggableManager
+
+
 
 # Create your models here.
+
+class Folder(models.Model):
+    tag = TaggableManager()
+
+    def __str__(self):
+        return self.name
+    
+
+
+class Bookmark(models.Model):
+    name = models.CharField(max_length=500)
+    url = models.URLField(max_length=200)
+    description = models.TextField()
+    frgn_key = models.ForeignKey(Folder, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
