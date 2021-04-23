110ff3d599c759fb9d61b6cc3741ff59c4a9a728
Rahul Sharma <bbblaze27@gmail.com>
2021-04-23 18:16:25 +0530

    Update models

--- bookmarkApp/models.py
+++ bookmarkApp/models.py
@@ -6,18 +6,18 @@ from taggit.managers import TaggableManager
 # Create your models here.
 
 class Folder(models.Model):
-    tag = TaggableManager()
+    folder_name = models.CharField(max_length=200)
 
     def __str__(self):
-        return self.name
+        return self.folder_name
     
-
 
 class Bookmark(models.Model):
     name = models.CharField(max_length=500)
     url = models.URLField(max_length=200)
     description = models.TextField()
-    frgn_key = models.ForeignKey(Folder, on_delete=models.CASCADE)
+    tag = TaggableManager()
+    folder = models.ForeignKey(Folder, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
