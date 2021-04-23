55bd26442346c000322025d64c294fde2080b125
Rahul Sharma <bbblaze27@gmail.com>
2021-04-23 18:14:04 +0530

    Update for adding django messages

--- Bookmark/settings.py
+++ Bookmark/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+from django.contrib.messages import constants as messages
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,4 +132,5 @@ STATIC_URL = '/static/'
 INSTALLED_APPS += [
     'bookmarkApp',
     'taggit',
+    'crispy_forms',
 ]
