2b78733880cbbbe599ce175a4a419faa98659106
Rahul Sharma <bbblaze27@gmail.com>
2021-04-24 23:14:19 +0530

    Add bookmark list view, detail view functions like -add -update -delete

--- bookmarkApp/urls.py
+++ bookmarkApp/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from . import views
+
+
 app_name = 'bookmarkApp'
 
 
 urlpatterns = [
-    path('', views.index, name = "index")
+    path('', views.Index.as_view(), name = "index"),
+    path('bookmark-detail/<int:pk>', views.BookmarkDetail.as_view(), name="bookmark-detail"),
+    path('add-bookmark', views.create_bookmark, name="add-bookmark"),
+    path('all-folders', views.FolderList.as_view(), name="all-folder"),
+    path('edit-bookmark/<int:pk>', views.edit_bookmark, name="edit-bookmark"),
+    path('delete-bookmark/<int:pk>', views.delete_bookmark, name="delete-bookmark"),
 ]
--- bookmarkApp/views.py
+++ bookmarkApp/views.py
@@ -1,7 +1,72 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import ListView, DetailView
+from .models import Bookmark, Folder
+from .forms import FolderForm, BookmarkForm
+from django.contrib import messages
 # Create your views here.
 
 
-def index(request):
-    return render(request, 'index.html')
+class Index(ListView):
+    model = Bookmark
+
+class BookmarkDetail(DetailView):
+    model = Bookmark
+
+class FolderList(ListView):
+    model = Folder
+
+
+
+def create_bookmark(request):
+    bookmark_form = BookmarkForm(request.POST or None)
+    folder_form = FolderForm(request.POST or None)
+
+    if bookmark_form.is_valid() and folder_form.is_valid():
+        bookmark = bookmark_form.save(commit=False)
+        folder, created = Folder.objects.get_or_create(folder_name=folder_form.cleaned_data['folder_name'])
+        bookmark.folder = folder
+        bookmark.save()
+        bookmark_form.save_m2m()
+        messages.success(request, 'Bookmark added succesfully')
+        return redirect('bookmarkApp:index')
+    
+    Context = {
+        'bookmark_form' : bookmark_form,
+        'folder_form' : folder_form,
+    }
+
+    return render(request, "bookmarkApp/add_bookmark.html" , context=Context)
+
+
+def edit_bookmark(request, pk):
+    bookmark = get_object_or_404(Bookmark, id=pk)
+    folder = get_object_or_404(Folder, id = bookmark.id)
+
+    bookmark_form = BookmarkForm(request.POST or None, instance=bookmark)
+    folder_form = FolderForm(request.POST or None, instance=folder)
+
+    if bookmark_form.is_valid() and folder_form.is_valid():
+        bookmark = bookmark_form.save(commit=False)
+        folder, created = Folder.objects.get_or_create(folder_name=folder_form.cleaned_data['folder_name'])
+        bookmark.folder = folder
+        bookmark.save()
+        bookmark_form.save_m2m()
+        messages.success(request, 'Bookmark edited succesfully')
+        return redirect('bookmarkApp:index')
+    
+    Context = {
+        'bookmark_form' : bookmark_form,
+        'folder_form' : folder_form,
+    }
+
+    return render(request, "bookmarkApp/add_bookmark.html" , context=Context)
+
+
+def delete_bookmark(request, pk):
+    bookmark = get_object_or_404(Bookmark, id=pk)
+    bookmark.delete()
+    messages.success(request, 'Bookmark deleted succesfully')
+    return redirect('bookmarkApp:index')
+
+
+
